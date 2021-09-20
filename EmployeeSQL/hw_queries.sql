9e2c6e6017e198648e50a750bec27458f472d626
saadandalib <85582682+saadandalib@users.noreply.github.com>
2021-09-20 10:34:16 -0700

    last_commit

---
+++ EmployeeSQL/hw_queries.sql
@@ -0,0 +1,76 @@
+-- Query 1
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees e
+INNER JOIN salaries s ON
+e.emp_no=s.emp_no;
+
+--Query 2
+SELECT e.emp_no, e.last_name, e.hire_date
+FROM employees e
+WHERE e.hire_date Like '%1986';
+
+--Query 3
+SELECT d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+FROM employees e
+INNER JOIN dept_manager dm ON
+e.emp_no=dm.emp_no
+INNER JOIN departments d ON
+d.dept_no=dm.dept_no;
+
+--Query 4
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp de ON
+e.emp_no=de.emp_no
+INNER JOIN departments d ON
+d.dept_no=de.dept_no;
+
+--Query 5 
+SELECT e.first_name, e.last_name, e.sex
+FROM employees e
+WHERE e.first_name = 'Hercules' AND e.last_name LIKE 'B%';
+
+--Query 6 
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp de ON
+e.emp_no=de.emp_no
+INNER JOIN departments d ON
+d.dept_no=de.dept_no
+WHERE e.emp_no IN
+	(
+	SELECT emp_no
+	FROM dept_emp dm
+	WHERE dept_no IN
+		(
+   		SELECT dept_no
+   		FROM departments d
+   		WHERE dept_name = 'Sales'
+ 		)
+	);
+	
+-- Query 7
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp de ON
+e.emp_no=de.emp_no
+INNER JOIN departments d ON
+d.dept_no=de.dept_no
+WHERE e.emp_no IN
+	(
+	SELECT emp_no
+	FROM dept_emp dm
+	WHERE dept_no IN
+		(
+   		SELECT dept_no
+   		FROM departments d
+   		WHERE dept_name = 'Sales' OR dept_name = 'Development'
+ 		)
+	);
+	
+-- Query 8
+Select e.last_name, COUNT(e.last_name) AS "Name Count"
+FROM employees e
+GROUP BY e.last_name
+ORDER BY "Name Count" DESC;
+
---
+++ EmployeeSQL/table_create.sql
@@ -0,0 +1,66 @@
+CREATE TABLE "departments" (
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+-- Address1 string
+-- Address2 NULL string
+-- Address3 NULL string
+CREATE TABLE "titles" (
+    "title_id" varchar   NOT NULL,
+    "title" varchar   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" varchar   NOT NULL,
+    "emp_title" varchar   NOT NULL,
+    "birth_date" varchar   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "sex" varchar   NOT NULL,
+    "hire_date" varchar   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" varchar   NOT NULL,
+    "dept_no" varchar   NOT NULL
+);
+
+-- TotalAmount money
+-- OrderStatusID int FK >- os.OrderStatusID
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar   NOT NULL,
+    "emp_no" varchar   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" varchar   NOT NULL,
+    "salary" int   NOT NULL
+);
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
